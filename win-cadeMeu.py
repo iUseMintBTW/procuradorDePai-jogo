ce75435b51b23e7855555e9e64a6281f706c3634
iUseMintBTW <120144344+iUseMintBTW@users.noreply.github.com>
2023-04-30 08:57:33 -0300

    Agora funciona no windows :3

---
+++ win-cadeMeu.py
@@ -0,0 +1,95 @@
+import time
+import sys, subprocess
+#import requests
+
+print("\n\t =======================================")
+print("\t|                                       |")
+print("\t| Seja bem viade ao encontrador de pai! |")
+print("\t|                                       |")
+print("\t =======================================")
+
+pai = input("\n\n\nMe diga o nome do teu pai: ")
+time.sleep(1)
+print("\n.")
+time.sleep(1)
+print(".")
+time.sleep(2)
+print('.')
+time.sleep(5)
+
+def encontrar(pai):
+  print("\n\n\n\t\tErro, " + pai.text + "não encontrado")
+
+
+subprocess.run('cls', shell=True)
+print("\n\n\n\n\t[                      ]\n\n")
+time.sleep(1)
+subprocess.run('cls', shell=True)
+print("\n\n\n\n\t[=                     ]\n\n")
+time.sleep(3)
+subprocess.run('cls', shell=True)
+print("\n\n\n\n\t[==                    ]\n\n")
+time.sleep(1)
+subprocess.run('cls', shell=True)
+
+
+
+
+print("\n\n\n\n\t[===                   ]")
+print("\t\tIsso pode levar algum tempo. Aguente firme! XD\n")
+time.sleep(5)
+subprocess.run('cls', shell=True)
+print("\n\n\n\n\t[====                  ]")
+print("\t\tIsso pode levar algum tempo. Aguente firme! XD\n")
+time.sleep(8)
+subprocess.run('cls', shell=True)
+print("\n\n\n\n\t[====                  ]")
+print("\t\tIsso pode levar algum tempo. Aguente firme! XD\n")
+time.sleep(1)
+subprocess.run('cls', shell=True)
+print("\n\n\n\n\t[=====                 ]")
+print("\t\tIsso pode levar algum tempo. Aguente firme! XD\n")
+time.sleep(1)
+subprocess.run('cls', shell=True)
+print("\n\n\n\n\t[======                ]")
+print("\t\tIsso pode levar algum tempo. Aguente firme! XD\n")
+time.sleep(1)
+subprocess.run('cls', shell=True)
+print("\n\n\n\n\t[=======               ]")
+print("\t\tIsso pode levar algum tempo. Aguente firme! XD\n")
+time.sleep(1)
+subprocess.run('cls', shell=True)
+print("\n\n\n\n\t[========              ]")
+print("\t\tIsso pode levar algum tempo. Aguente firme! XD\n")
+time.sleep(1)
+subprocess.run('cls', shell=True)
+print("\n\n\n\n\t[=========             ]")
+print("\t\tIsso pode levar algum tempo. Aguente firme! XD\n")
+time.sleep(13)
+subprocess.run('cls', shell=True)
+
+
+
+print("\n\n\n\n\t[==========            ]")
+print("\t\tIsso pode levar algum tempo. Aguente firme! XD\n")
+time.sleep(1)
+subprocess.run('cls', shell=True)
+print("\n\n\n\n\t[===========           ]")
+print("\t\tIsso pode levar algum tempo. Aguente firme! XD\n")
+time.sleep(1)
+subprocess.run('cls', shell=True)
+print("\n\n\n\n\t[============          ]")
+print("\t\tQuase lá...\n")
+time.sleep(21)
+subprocess.run('cls', shell=True)
+
+
+print("\n\n\n\n\t[===================== ]\n")
+print("\t\tProcuração concluida! ^^\n\n")
+time.sleep(2)
+
+
+
+
+encontrar(pai)
+
